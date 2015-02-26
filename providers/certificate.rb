cbd4a4649be58337d098ceb83f9fb5aff219c3d0
Nick McSpadden <nmcspadden@gmail.com>
2015-02-26 13:46:39 -0800

    Whoops

--- providers/certificate.rb
+++ providers/certificate.rb
@@ -181,7 +181,7 @@ action :create do
       # write out the csr to disk
       unless csr.nil?
         f = resource("file[#{csr_path}]")
-        f.content csr
+        f.content csr.to_pem
         f.action :create
       end
 
