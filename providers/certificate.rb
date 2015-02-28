2f1087b837719e79080a4272d45a9491b744a40f
Nick McSpadden <nmcspadden@gmail.com>
2015-02-28 12:32:47 -0800

    Fixed typecasting issue

--- providers/certificate.rb
+++ providers/certificate.rb
@@ -47,7 +47,8 @@ action :create do
     end
   end
 
-  name_sha = Digest::SHA256.new << new_resource.name + node["name"]
+  name_sha_combined = new_resource.node + node["name"]
+  name_sha = Digest::SHA256.new << new_sha_combined
   cert_id = name_sha.to_s
 
   # Try to find this certificate in the data bag.
